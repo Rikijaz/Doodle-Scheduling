5d6bbb3edc68d606f72bab964ce8f245fd31ab62
Yulin Zhang <32946065+lannshine@users.noreply.github.com>
2019-12-03 00:40:34 -0800

    Update Login.js
    Add editable name and bio, add upload profile picture (stored at Firebase Storage).
    To do: update Storage security rules

--- doodle_scheduling/src/components/Login.js
+++ doodle_scheduling/src/components/Login.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 import logo from "./logo.png";
 import { Button } from "@material-ui/core";
-
-// import firebase from "firebase";
+import Header from "./header";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-
 import { Link } from "react-router-dom";
-
 import { firebase, db } from "./firebase";
+import FileUploader from "react-firebase-file-uploader";
+import { textAlign } from "@material-ui/system";
 
 var CLIENT_ID = "YOUR_OAUTH_CLIENT_ID";
+/*
 const logoStyle = {
     textAlign: "center"
 };
-
+*/
 const logInStyle = {
     textAlign: "center",
     top: "100%",
@@ -47,9 +47,37 @@ export class Login extends Component {
         super(props);
         this.state = {
             isSignedIn: false,
-			picURL: ""
+            picURL: "",
+            nameDisplay: "",
+            current_user_email: "",
+            userName: "",
+            bioDisplay: "",
+            userBio: "",
+            isUploading: false,
+            progress: 0,
+            profilePic: ""
         };
     }
+
+    // handle profile pic upload to Storage
+    handleUploadStart = () => this.setState({ isUploading: true, progress: 0 });
+
+    handleProgress = progress => this.setState({ progress });
+
+    handleUploadError = error => {
+        this.setState({ isUploading: false });
+        console.error(error);
+    };
+
+    handleUploadSuccess = filename => {
+        this.setState({ profilePic: filename, progress: 100, isUploading: false });
+        firebase
+            .storage()
+            .ref("images")
+            .child(filename)
+            .getDownloadURL()
+            .then(url => this.setState({ picURL: url }));
+    };
 
     componentDidMount = () => {
         firebase.auth().onAuthStateChanged(user => {
@@ -68,6 +96,7 @@ export class Login extends Component {
                             events: [],
                             polls: [],
                             pictureURL: user.photoURL,
+                            bio: ""
                         });
                     }
                 });
@@ -79,6 +108,11 @@ export class Login extends Component {
                     } else {
                         //console.log('Document data:', doc.data());
                         this.setState({ picURL: doc.data().pictureURL });
+                        this.setState({ nameDisplay: doc.data().displayName });
+                        this.setState({ userName: doc.data().displayName });
+                        this.setState({ current_user_email: doc.data().email });
+                        this.setState({ bioDisplay: doc.data().bio });
+                        this.setState({ userBio: doc.data().bio });
                     }
                 })
                 .catch(err => {
@@ -98,32 +132,89 @@ export class Login extends Component {
         localStorage.clear();
     };
 
+    //update profile info in db
+    updateName = e => {
+        this.setState({
+            nameDisplay: e.target.value
+        });
+    }
+    
+    updateBio = e => {
+        this.setState({
+            bioDisplay: e.target.value
+        });
+    }
+
+    updateDb = e => {
+        if (this.state.nameDisplay !== this.state.userName) {
+            db.collection("users")
+                .doc(this.state.current_user_email)
+                .update({
+                    displayName: this.state.nameDisplay,
+                });
+        }
+        if (this.state.bioInput !== "") {
+            db.collection("users")
+                .doc(this.state.current_user_email)
+                .update({
+                    bio: this.state.bioDisplay
+                });
+        }
+        db.collection("users")
+            .doc(this.state.current_user_email)
+            .update({
+                pictureURL: this.state.picURL
+            });
+    };
+
     render() {
         return (
-            <div>
-                <div style={logoStyle}>
-                    <header>
-                        <img src={logo} alt="Schedule It" />
-                    </header>
-                </div>
+            <div style={{ textAlign: "center" }}>
+                <Header />
                 {this.state.isSignedIn ? (
                     <div style={logInStyle}>
-                        <h3>
-							<img src={this.state.picURL} alt="Profile" vertical-align="middle" width="100px" height="100px" border-radius="50%" />
+                        <img src={this.state.picURL} alt="Profile" vertical-align="middle" width="100px" height="100px" border-radius="50%" />
+                        <br />                        
+                        <form onSubmit={this.updateDb}>
+                            {this.state.isUploading && <p>Progress: {this.state.progress}</p>}
+                            <FileUploader
+                                accept="image/*"
+                                name="Profile Picture"
+                                randomizeFilename
+                                storageRef={firebase.storage().ref("images")}
+                                onUploadStart={this.handleUploadStart}
+                                onUploadError={this.handleUploadError}
+                                onUploadSuccess={this.handleUploadSuccess}
+                                onProgress={this.handleProgress}
+                            />
                             <br />
-							Name:
-                            {firebase.auth().currentUser.displayName}
+                            <label>{firebase.auth().currentUser.email}</label>
                             <br />
-                            email: {firebase.auth().currentUser.email}
-                        </h3>
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            component={Link}
-                            to="/home"
-                        >
-                            Go to Home Page
-                        </Button>
+                            <label>Name</label>
+                            <br />
+                            <input
+                                type="text"
+                                name="fullname"
+                                size="26"
+                                placeholder="Your name"
+                                onChange={e => this.setState({ nameDisplay: e.target.value })}
+                                value={this.state.nameDisplay}
+                            />
+                            <br />
+                            <label>Biography</label>
+                            <br />
+                            <textarea
+                                name="bioText"
+                                rows="10"
+                                cols="27"
+                                placeholder="Biography"
+                                value={this.state.bioDisplay}
+                                onChange={e => this.setState({ bioDisplay: e.target.value })}
+                            >
+                            </textarea>
+                            <br/>
+                            <button type="submit">Save changes</button>
+                        </form>
                         <br />
                         <Button
                             variant="contained"
