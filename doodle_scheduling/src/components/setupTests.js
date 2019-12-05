5fd6fbf5739a255539a9ed58b9f5420ee5b3f414
rikijaz <rikijaz34@gmail.com>
2019-12-05 10:24:17 -0800

    Revert "i tried to test the initializations of the class constructor this.state variables, AddEvent.js is a good test file to look at. some of the test files have just been copied and created"
    This reverts commit d79afa77416487e56eb016631d3ea966704abfda.

--- doodle_scheduling/src/components/setupTests.js
+++
@@ -1,8 +0,0 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-const enzyme = require("enzyme");
-const Adapter = require("enzyme-adapter-react-16");
-
-enzyme.configure({ adapter: new Adapter() });
-// configure({ adapter: new Adapter() });
