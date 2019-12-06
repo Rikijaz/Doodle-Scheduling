f2ba6ede496791b15ee7518e365d26e59b70438e
rikijaz <rikijaz34@gmail.com>
2019-12-06 15:45:45 -0800

    Modified bash script to loop through all .test.js files and test them

--- doodle_scheduling/tests.sh
+++ doodle_scheduling/tests.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-echo "Running all tests."
+echo "Running tests."
+
+echo "Installing dependencies."
 
 npm install
 
+echo "Dependencies installed."
+
+echo "Running all tests."
+
 cd doodle_scheduling/src/components/
 
-npm test AddEvent.test.js
+find src/components -type f -name \*.test.js | while read line; do
+    echo "Testing' $line'"
+    npm test $line || continue
+done
+
+echo "Ran all tests."
